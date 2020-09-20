7305b1be77025c90be3f6884949ac0ca003fcbd0
bobby.li <lihan0603@hupu.com>
2020-09-20 21:42:02 +0800

    change ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,19 +20,19 @@ module.exports = {
         API_ENV: 'prod',
         PORT: 6600,
       },
-      log_file: '/home/ubuntu/var/log/omni-door/site.log',
-      error_file: '/home/ubuntu/var/log/omni-door/site-err.log',
-      out_file: '/home/ubuntu/var/log/omni-door/site-out.log',
+      log_file: '/home/bobby/var/log/omni-door/site.log',
+      error_file: '/home/bobby/var/log/omni-door/site-err.log',
+      out_file: '/home/bobby/var/log/omni-door/site-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm Z' // 设置日志的日期格式
     }
   ],
   deploy: {
     production: {
-      user: 'ubuntu',
+      user: 'bobby',
       host: '13.78.66.217',
       ref: 'origin/master',
       repo: 'git@github.com:omni-door/site.git',
-      path: '/home/ubuntu/www/omni-door',
+      path: '/home/bobby/www/omni-door',
       key: '~/.ssh/azure-omni_key.pem',
       'post-deploy': 'git pull && yarn reload',
       env: {
